ef9970c5c66f56dd22929ab23a7e35b3617cb6cf
bhartendra <bhartendra.singh55@gmail.com>
2018-02-12 16:14:17 +0530

    it is showing HTTP Status 404

--- src/webapp/helloworld.java
+++ src/webapp/helloworld.java
@@ -3,17 +3,15 @@ package webapp;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet(urlPatterns="/helloworld")
-public class Helloworld extends HttpServlet {
+
+public class helloworld extends HttpServlet {
 	protected void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException,ServletException {
 		request.setAttribute("message", "Hello World");
-		request.getRequestDispatcher("/showhelloWorld").forward(request,response);
+		request.getRequestDispatcher("/showhelloWorld.jsp").forward(request,response);
 		
 	}
 	
-
 }
